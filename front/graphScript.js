4d0958fbc44190bb4e1a1d1d0d1816ea303eb1f5
Andrei Villa <34303451+Andreivilla@users.noreply.github.com>
2023-06-30 15:39:54 -0300

    graficos

---
+++ front/graphScript.js
@@ -0,0 +1,108 @@
+    // Função para fazer a requisição do arquivo JSON
+    function loadJSON(callback) {
+        const xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open("GET", "servidorquedevemandar/exercicios.json", true);
+        xobj.onreadystatechange = function() {
+          if (xobj.readyState === 4 && xobj.status === 200) {
+            callback(xobj.responseText);
+          }
+        };
+        xobj.send(null);
+      }
+  
+      // Função para criar o gráfico com base nos dados do JSON
+      function createChart(jsonData, exercicio) {
+        // Dados do JSON
+        const data = JSON.parse(jsonData);
+  
+        // Encontrar o objeto do exercício selecionado
+        const exercicioData = data.historico_cargas.find(item => item.nome === exercicio);
+  
+        // Obter as datas e volumes do exercício selecionado
+        const datas = exercicioData.datas;
+        const volumes = exercicioData.volume;
+  
+        // Verificar se o gráfico já existe e destruí-lo antes de criar um novo
+        const existingChart = Chart.getChart("myChart");
+        if (existingChart) {
+          existingChart.destroy();
+        }
+  
+        // Criar gráfico de colunas
+        const ctx = document.getElementById("myChart").getContext("2d");
+        new Chart(ctx, {
+          type: "bar",
+          data: {
+            labels: datas,
+            datasets: [{
+              label: exercicio,
+              data: volumes,
+              backgroundColor: getRandomColor(), // Função auxiliar para gerar cores aleatórias
+              borderWidth: 1
+            }]
+          },
+          options: {
+            responsive: true,
+            title: {
+              display: true,
+              text: "Volume de cargas por exercício"
+            },
+            scales: {
+              y: {
+                beginAtZero: true,
+                title: {
+                  display: true,
+                  text: "Volume"
+                }
+              },
+              x: {
+                title: {
+                  display: true,
+                  text: "Data"
+                }
+              }
+            }
+          }
+        });
+      }
+  
+      // Função auxiliar para gerar cores aleatórias
+      function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+  
+      // Função para atualizar o gráfico quando o exercício selecionado é alterado
+      function updateChart() {
+        const exercicioSelect = document.getElementById("exercicioSelect");
+        const exercicioSelecionado = exercicioSelect.value;
+  
+        // Carregar o JSON e criar o gráfico com base no exercício selecionado
+        loadJSON(function(response) {
+          createChart(response, exercicioSelecionado);
+        });
+      }
+  
+      // Carregar o JSON e criar o gráfico inicial com base nos exercícios disponíveis
+      loadJSON(function(response) {
+        const exercicioSelect = document.getElementById("exercicioSelect");
+        const data = JSON.parse(response);
+        const exercicios = data.historico_cargas;
+  
+        // Preencher o seletor com as opções de exercício
+        exercicios.forEach(function(exercicio) {
+          const option = document.createElement("option");
+          option.value = exercicio.nome;
+          option.text = exercicio.nome;
+          exercicioSelect.appendChild(option);
+        });
+  
+        // Selecionar o primeiro exercício por padrão
+        const exercicioSelecionado = exercicioSelect.value;
+        createChart(response, exercicioSelecionado);
+      });
